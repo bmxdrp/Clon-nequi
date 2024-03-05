9e67757af2589048563833123e9874747f4bdcef
Bmxdrp <reenvia@outlook.com>
2024-03-04 20:34:24 -0500

    Clon Nequi Educativo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			background: {
+				
+			},
 			colors: {
 				primary_uva: '#200020',
 				midnight: "#210049",
